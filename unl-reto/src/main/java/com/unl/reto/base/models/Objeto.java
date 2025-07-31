cea72e4576f502607c8137af3c717e1c07646cf0
Dougdree <douglas.carreno@unl.edu.ec>
2025-07-31 09:34:42 -0500

    claes

--- unl-reto/src/main/java/com/unl/reto/base/models/Objeto.java
+++ unl-reto/src/main/java/com/unl/reto/base/models/Objeto.java
@@ -1,1 +1,50 @@
 package com.unl.reto.base.models;
+
+public class Objeto {
+    private Integer id;
+    private String nombre;
+    private TipoObjeto tipo;
+    private TamañoObjeto tamano;
+    private Cajon cajon;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public TipoObjeto getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(TipoObjeto tipo) {
+        this.tipo = tipo;
+    }
+
+    public TamañoObjeto getTamano() {
+        return this.tamano;
+    }
+
+    public void setTamano(TamañoObjeto tamano) {
+        this.tamano = tamano;
+    }
+
+    public Cajon getCajon() {
+        return this.cajon;
+    }
+
+    public void setCajon(Cajon cajon) {
+        this.cajon = cajon;
+    }
+
+}
--- unl-reto/src/main/java/com/unl/reto/base/models/TipoAccion.java
+++
@@ -1,5 +0,0 @@
-package com.unl.reto.base.models;
-
-public enum TipoAccion {
-    ELIMINAR_DUPLICADOS, ORDENAR_POR_TIPO, ORDENAR_POR_TAMANO
-}
