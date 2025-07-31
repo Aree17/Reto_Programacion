cf5e6c03850d86742e7db6684a58035cea068148
Arelys Ajila <arelys.ajila@unl.edu.ec>
2025-07-31 09:45:49 -0500

    Metodo delete y ajuste en pom

--- unl-reto/src/main/java/com/unl/reto/base/controller/dao/dao_models/DaoCajon.java
+++ unl-reto/src/main/java/com/unl/reto/base/controller/dao/dao_models/DaoCajon.java
@@ -1,5 +1,71 @@
 package com.unl.reto.base.controller.dao.dao_models;
 
-public class DaoCajon {
+import com.unl.reto.base.controller.dao.AdapterDao;
+import com.unl.reto.base.models.Cajon;
+
+public class DaoCajon extends AdapterDao<Cajon> {
+    private Cajon obj;
+
+    public DaoCajon(){
+        super(Cajon.class);
+    }
+
+    public Cajon getObj() {
+        if(obj==null)
+            this.obj=new Cajon();
+        return this.obj;
+    }
+
+    
+    public void setObj(Cajon obj) {
+        this.obj = obj;
+    }
+
+    public Boolean save(){
+        try{
+            this.persist(obj); 
+            return true;
+
+        }catch(Exception e){
+            e.printStackTrace(); 
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public Boolean update(Integer pos){
+        try{
+            obj.setId(listAll().getLength()+1);
+            this.update(obj, pos);
+            return true;
+
+        }catch(Exception e){
+            e.printStackTrace(); 
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public Boolean deletePersona(Integer id) {
+        try {
+            super.delete(id);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        DaoCajon da= new DaoCajon();
+        da.getObj().setId(da.listAll().getLength()+1);
+        if(da.save())
+            System.out.println("GUARDADO");
+        else
+        System.out.println("Error");
+    }
+
 
 }
